1debf6f9806dff6f4d8951d5e4dc214967776403
Govind <govind@Govinds-Mac.local>
2019-11-25 22:16:05 +0530

    Updates

---
+++ precache-manifest.1e2582f60881f0cb0dad75e5200fa1ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c67be2b2591326a676071e594ab3b7a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "14932e2157930bad02f3",
+    "url": "/robofriends/static/css/main.261598cd.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "14932e2157930bad02f3",
+    "url": "/robofriends/static/js/main.6363150f.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
